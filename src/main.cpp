d1a9742d0d37d18f47de91be6049f06dba68b6d1
beta-tank <beta-tank@outlook.com>
2018-12-17 21:11:59 +0700

    Update deps. Add wi-fi and OTA init code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,67 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
+
+#define MODULE_NAME "esp8266-climate-station"
+
+void initWiFi();
+void initOTA();
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.print("[");
+  Serial.print(MODULE_NAME);
+  Serial.println("] Start setup");
+  initWiFi();
+  initOTA();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ArduinoOTA.handle();
+  yield();
+}
+
+void initWiFi(){
+  WiFi.mode(WIFI_STA);
+  WiFi.setAutoConnect(true);
+  WiFi.setAutoReconnect(true);
+  Serial.println("");
+  Serial.print("ssid: ");
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(WiFi.status());
+  }
+  
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void initOTA(){
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(MODULE_NAME);
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start OTA");
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("End OTA");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
 }
